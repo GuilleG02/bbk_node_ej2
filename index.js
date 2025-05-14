859786a68347525a0051dc25c00610a9197da2bf
GuilleG02 <guille.g0220rgmail.com>
2025-05-14 20:05:27 +0200

    Ejercicio

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const logplease = require("logplease");
+
+const logger = logplease.create("bbk_node_ej2");
+
+logger.debug("Debug");
+logger.info("Info");
+logger.warn("Warning");
+logger.error("Error");
+
